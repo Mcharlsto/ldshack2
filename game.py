be2a9a8eb2bf8a8c11aeb98aafe670c8a948a44d
Mcharlsto <matthewcharlston@sky.com>
2018-10-06 14:01:22 +0000

    Update

--- game.py
+++ game.py
@@ -4,17 +4,34 @@ import time
 
 game = Game()
 
+def ifWord(word, check):
+    ifWord = False
+    if word.lower() == check:
+        ifWord = True
+    else:
+        ifWord = False
+    return ifWord
+
 def listen(self, actor, noun, words):
     print "What is the worst vegetable to have on a ship?"
     print "Enter answer"
+    global input
     input = raw_input()
-    if input == "leek":
+    if ifWord(input, "leek"):
         print "You Win. The frog drops a key. You can use \"take key\" to collect it."
         side4key = side4.new_object("key", "Key to the Elevator")
         floor3.make_requirement(side4key)
     else:
         print "You Lose"
         listen(0,0,0,0)
+    if ifWord(input, "a leek"):
+        print "You Win. The frog drops a key. You can use \"take key\" to collect it."
+        side4key = side4.new_object("key", "Key to the Elevator")
+        floor3.make_requirement(side4key)
+    else:
+        print "You Lose"
+        listen(0,0,0,0)
+
 
 floor5 = game.new_location(
 "Floor 5",
