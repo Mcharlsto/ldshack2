26b46e7d744af1a605cf2af8a3f06e86a8f7a400
jam772 <43905775+jam772@users.noreply.github.com>
2018-10-07 11:50:26 +0100

    End Screen Code

---
+++ end_screen.py
@@ -0,0 +1,21 @@
+import webbrowser
+import time
+print("Well done! \nYou completed the Libary Escape Game!\n\n\n")
+print("""|@@@@|     |####|
+|@@@@|     |####|
+|@@@@|     |####|
+\@@@@|     |####/
+ \@@@|     |###/
+  `@@|_____|##'
+       (O)
+    .-'''''-.
+  .'  * * *  `.
+ :  *       *  :
+: ~   Libary ~  :
+: ~   Escape ~  :
+ :  *       *  :
+  `.  * * *  .'
+    `-.....-'\n\n\n""")
+print("Thank you for playing!")
+time.sleep(5)
+webbrowser.open("https://jam772.github.io/")
